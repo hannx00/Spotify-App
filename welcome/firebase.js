12e687448df5ed329f05b4b4bed1500f05e0ec29
Ang Li-Lian <anglilian@minerva.kgi.edu>
2022-01-16 16:19:39 +0100

    merge firebase + welcome

---
+++ welcome/firebase.js
@@ -0,0 +1,94 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAIsY0Fm4ivag8quUpDzr9J8aEmlownfBs",
+    authDomain: "login-data-4b9eb.firebaseapp.com",
+    projectId: "login-data-4b9eb",
+    storageBucket: "login-data-4b9eb.appspot.com",
+    messagingSenderId: "136238214659",
+    appId: "1:136238214659:web:75cabdd0249ac1545a2836",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Initialize variables
+  const auth = firebase.auth()
+  const database = firebase.database()
+  
+  // Set up our register function
+  function register () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+
+    // Move on with Auth
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        email : email,
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).set(user_data)
+  
+      // DOne
+      alert('User Created!!')
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+  
+  // Set up our login function
+  function login () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+  
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).update(user_data)
+  
+      // DOne
+      alert('User Logged In!!')
+  
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+
+// authenticateSpotify(){
+//     fetch('/spotify/is-authenticated').then((response)=> response.json()).then((data)=>{this.setState({spotifyAuthenticated: data.status})});
+//     if (!data.status){
+//       fetch('/spotify/get-auth-url').then((response)=>response.json()).then((data)=>{
+//         window.location.replace(data.url);
+//       })
+//     }
+//   }
